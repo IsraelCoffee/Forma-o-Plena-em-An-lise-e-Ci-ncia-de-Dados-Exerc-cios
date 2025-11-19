4c878966d3e66fdc5417a7532b20d5967152053b
Israel Lisbôa <240237669+IsraelCoffee@users.noreply.github.com>
2025-11-19 18:23:41 -0300

    Update R Prático I.R

--- R Prático I.R
+++ R Prático I.R
@@ -1,3 +1,19 @@
+##crie a funcao {
+  #crie uma lista de valores(
+    ##calcule a média e guarde como media,
+    #calcule a mediana e guarde como mediana,
+    #calcule o valor máximo e guarde como maximo,
+    #calcule o valor mínimo e guarde como menor
+  )
+}
+
+# Exemplo de uso:
+#crie o vetor_teste
+#calcule o resultado usando o vetor da funcao e selve como resultado
+
+# Impressão dos resultados
+#mostre o resultado usando print
+
 estatisticas_vetor <- function(vetor) {
   lista_resultado <- list(
     media = mean(vetor),
@@ -9,3 +25,4 @@ estatisticas_vetor <- function(vetor) {
   return(lista_resultado)
 }
 estatisticas_vetor(c(1, 5, 3, 9, 2))
+
